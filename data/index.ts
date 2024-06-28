6eea57c80362f6994b1adc8aeb7110111f6da532
shivaprasad369 <122955366+shivaprasad369@users.noreply.github.com>
2024-06-28 21:27:57 +0530

    Add

---
+++ data/index.ts
@@ -0,0 +1,221 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#projects" },
+    { name: "Testimonials", link: "#testimonials" },
+    { name: "Contact", link: "#contact" },
+  ];
+  
+  export const gridItems = [
+    {
+      id: 1,
+      title: "I prioritize client collaboration, fostering open communication ",
+      description: "",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+      imgClassName: "w-full h-full",
+      titleClassName: "justify-end",
+      img: "/b1.svg",
+      spareImg: "",
+    },
+    {
+      id: 2,
+      title: "We are very flexible with time zone communications",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 3,
+      title: "Our tech stack",
+      description: "We constantly try to improve",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-center",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 4,
+      title: "Tech enthusiast with a passion for development.",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "/grid.svg",
+      spareImg: "/b4.svg",
+    },
+  
+    {
+      id: 5,
+      title: "Currently building a JS Animation library",
+      description: "The Inside Scoop",
+      className: "md:col-span-3 md:row-span-2",
+      imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+      titleClassName: "justify-center md:justify-start lg:justify-center",
+      img: "/b5.svg",
+      spareImg: "/grid.svg",
+    },
+    {
+      id: 6,
+      title: "Do you want to start a project together?",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+      img: "",
+      spareImg: "",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 1,
+      title1: "QuickResponseDrive",
+      title: "Quick Response Drive",
+      des: " dynamic multi-vendor marketplace, where innovation meets convenience. We're committed to empowering sellers and delighting shoppers with a seamless online shopping experience.",
+      img: "/p1.png",
+      iconLists: ["/re.svg", "/tail.svg",  "/fm.svg"],
+      link: "https://quickresponsedriver.shop/?i=1",
+    },
+    {
+      id: 2,
+      title1: "Neoweds",
+      title: "Neoweds",
+      des: "The NEOWEDS crew which is team of 83 well professional photographers has photographed over 800 weddings and over 300 engagement photos for different couples from throughout the nation.",
+      img: "/p2.png",
+      iconLists: [ "/tail.svg", "/fm.svg", "/re.svg"],
+      link: "https://neowedss.netlify.app/",
+    },
+    {
+      id: 3,
+      title1: "EventEleganceHub",
+      title: "Event Elegance Hub",
+      des: "Delve into the Extraordinary with Our Event Coordination Expertise. From Concept to Celebration, We Make Every Moment Memorable",
+      img: "/p3.png",
+      iconLists: ["/re.svg", "/tail.svg", "/fm.svg", ],
+      link: "https://eventelegancehub.online/?i=2",
+    },
+    {
+      id: 4,
+      title1: "SriGaneshTours&Travels",
+      title: "SRI GANESH TOURS & TRAVELS",
+      des: "Starting a car rental company involves thorough market research to identify target customers and competitors. Key steps include acquiring a diverse fleet of vehicles, obtaining necessary licenses and insurance, and setting up an efficient booking system.",
+      img: "/p4.png",
+      iconLists: ["/re.svg", "/tail.svg", "/fm.svg", ],
+      link: "https://sgthubli.com/",
+    },
+  ];
+  
+  export const testimonials = [
+    {
+      quote:
+        "What truly sets CodeCrafters apart is their commitment to customer satisfaction. They went above and beyond to ensure we were happy with the results, providing ongoing support and maintenance even after the project was completed.",
+      name: "Rajesh",
+      title: "Customer ",
+    },
+    {
+      quote:
+        "Communication was seamless throughout the process. They kept us informed at every stage and were always available to answer any questions or address concerns promptly. The project was completed on time and within budget, exceeding our expectations in every way.",
+      name: "Vijay Hulagur",
+      title: "Customer",
+    },
+    {
+      quote:
+        "I recently had the pleasure of working with [Company Name], and I couldn't be more impressed with their services. From the initial consultation to the final implementation, their team demonstrated exceptional professionalism, expertise, and dedication.",
+      name: "Girish Rajur",
+      title: "Customer",
+    },
+    {
+      quote:
+        "The CodeCrafters team took the time to thoroughly understand our needs and provided tailored solutions that perfectly aligned with our business goals. Their technical proficiency is unmatched, and they handled every aspect of our project with precision and care.",
+      name: "Shivakumar",
+      title: "Customer",
+    },
+    // {
+    //   quote:
+    //     "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    //   name: "Michael Johnson",
+    //   title: "Director of AlphaStream Technologies",
+    // },
+  ];
+  
+  export const companies = [
+    {
+      id: 1,
+      name: "cloudinary",
+      img: "/cloud.svg",
+      nameImg: "/cloudName.svg",
+    },
+    {
+      id: 2,
+      name: "appwrite",
+      img: "/app.svg",
+      nameImg: "/appName.svg",
+    },
+    {
+      id: 3,
+      name: "HOSTINGER",
+      img: "/host.svg",
+      nameImg: "/hostName.svg",
+    },
+    {
+      id: 4,
+      name: "stream",
+      img: "/s.svg",
+      nameImg: "/streamName.svg",
+    },
+    {
+      id: 5,
+      name: "docker.",
+      img: "/dock.svg",
+      nameImg: "/dockerName.svg",
+    },
+  ];
+  
+  export const workExperience = [
+    {
+      id: 1,
+      title: "Web Application Development",
+      desc: "Developing scalable and robust applications with custom features and functionalities.",
+      className: "md:col-span-2",
+      thumbnail: "/exp1.svg",
+    },
+    {
+      id: 2,
+      title: "Mobile App Development",
+      desc: "Building tailored mobile applications to meet specific business requirements.",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp2.svg",
+    },
+    {
+      id: 3,
+      title: "Digital marketing",
+      desc: "Digital marketing encompasses various online strategies and techniques to promote products or services, social media, email, and websites.",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp3.svg",
+    },
+    {
+      id: 4,
+      title: "IT Support Services",
+      desc: "providing technical assistance and troubleshooting to ensure the smooth operation of hardware, software, and network systems for businesses and individuals.",
+      className: "md:col-span-2",
+      thumbnail: "/exp4.svg",
+    },
+  ];
+  
+  export const socialMedia = [
+    {
+      id: 1,
+      img: "/git.svg",
+    },
+    {
+      id: 2,
+      img: "/twit.svg",
+    },
+    {
+      id: 3,
+      img: "/link.svg",
+    },
+  ];
